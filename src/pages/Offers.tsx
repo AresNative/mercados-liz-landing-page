c66d9c696a59c2bb8b22624f9fe1e4f184acfc71
Juan Jose Eusebio Rodriguez <sistemas02@mercadosliz.com>
2024-09-23 11:13:34 -0700

    ajuste de diseño responsive y ajuste en pantalla de ofertas

--- src/pages/Offers.tsx
+++ src/pages/Offers.tsx
@@ -1,10 +1,53 @@
+import { IconCard } from "@/components/displays/card-icon";
 import Page from "@/template/page";
+import { ShoppingCart } from "lucide-react";
+import Styles from "./Offers.module.css";
 const Offers = () => {
     return (
         <Page titulo="Ofertas">
 
-            <>
-            </>
+            <nav className={Styles["nav"]}>
+                <ul className={Styles["ul"]}>
+                    <li>
+                        <IconCard title='Selección Local' text='Productos auténticos del Valle de Guadalupe' >
+                            <ShoppingCart color='var(--primary)' size={40} />
+                        </IconCard>
+                    </li>
+                    <li>
+                        <IconCard title='Selección Local' text='Productos auténticos del Valle de Guadalupe' >
+                            <ShoppingCart color='var(--primary)' size={40} />
+                        </IconCard>
+                    </li>
+                    <li>
+                        <IconCard title='Selección Local' text='Productos auténticos del Valle de Guadalupe' >
+                            <ShoppingCart color='var(--primary)' size={40} />
+                        </IconCard>
+                    </li>
+                    <li>
+                        <IconCard title='Selección Local' text='Productos auténticos del Valle de Guadalupe' >
+                            <ShoppingCart color='var(--primary)' size={40} />
+                        </IconCard>
+                    </li>
+                </ul>
+
+                <ul className={Styles["ul"]}>
+                    <li>
+                        <IconCard title='Selección Local' text='Productos auténticos del Valle de Guadalupe' >
+                            <ShoppingCart color='var(--primary)' size={40} />
+                        </IconCard>
+                    </li>
+                    <li>
+                        <IconCard title='Selección Local' text='Productos auténticos del Valle de Guadalupe' >
+                            <ShoppingCart color='var(--primary)' size={40} />
+                        </IconCard>
+                    </li>
+                    <li>
+                        <IconCard title='Selección Local' text='Productos auténticos del Valle de Guadalupe' >
+                            <ShoppingCart color='var(--primary)' size={40} />
+                        </IconCard>
+                    </li>
+                </ul>
+            </nav>
 
         </Page>
     )

b9552d9a5a9d6e4197aecc9616590fb6740d813a
Juan José Eusebio <juanerpgr@gmail.com>
2025-02-07 17:17:21 -0800

    Agregar encabezado para el formato de subida en la página de postulaciones

--- src/pages/test/@test/page.tsx
+++ src/pages/test/@test/page.tsx
@@ -5,7 +5,10 @@ export default function PageTestMaquetado() {
 
     return (
         <Background>
-            <></>
+
+            <section className="w-4/5 m-auto" >
+                <h1>Formato de subida para postulaciones</h1>
+            </section>
         </Background>
     );
 }
